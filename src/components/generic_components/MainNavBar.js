2b3719347621cee06cd3417d950e511b9976470d
Guillermo Fernández-Campoamor Fernández <uo264207@uniovi.es>
2020-02-25 22:41:21 +0100

    Minimal UIX include to GenericButton
    TODO: Brand tag VIADE ---> Needs a better "style"
                Add Flex or Grid Layout to the NavBar

--- src/components/generic_components/MainNavBar.js
+++ src/components/generic_components/MainNavBar.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import '../../assets/css/MainNavBar.css';
+import GenericButton from './GenericButton';
 
 //I used bootstrap just not to leave a white nav bar on this commit :)
 class MainNavBar extends Component{
@@ -6,14 +8,26 @@ class MainNavBar extends Component{
 		return (
 		  	<nav class="navbar navbar-inverse">
 		  	<div class="container-fluid">
-			  
 			  	<div class="navbar-header">
-				  <a class="navbar-brand" href="">{this.props.companyName}</a>
+                    <button type ="button" class="navbar-toggle" data-toggle="collapse"
+				     data-target="#myNavbar">
+				        <span>| | |</span>
+			        </button>
+                    <div class="brandContainer">
+				        <a class="navbar-brand" href="">
+                            <span>{this.props.companyName}</span>
+                        </a>
+                    </div>
 			  	</div>
-
-			  <ul class="nav navbar-nav">				  
-
-			  </ul>
+                <div class ="collapse navbar-collapse"id="myNavbar">
+			        <ul class ="nav navbar-nav navbar-right">	  
+                       <GenericButton 
+                        className="buttonGenBlue"
+                        name="buttomNavLogin"
+                        message="Log In"
+                        />
+			        </ul>
+                </div>
 		  	</div>
 		  	</nav>
 		);
