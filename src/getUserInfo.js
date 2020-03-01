6a2a07f5207fb2c38109a58e5b71ed201d8812da
Miguel <miguel111max@outlook.es>
2020-03-01 20:58:56 +0100

    Some minor changes just to merge things that i need

---
+++ src/getUserInfo.js
@@ -0,0 +1,22 @@
+import auth from 'solid-auth-client';
+
+async function getWebId() {
+    /* 1. Check if we've already got the user's WebID and access to their Pod: */
+    let session = await auth.currentSession();
+    if (session) {
+        return session.webId;
+    }
+
+    /* 2. User has not logged in; ask for their Identity Provider: */
+    // Implement `getIdentityProvider` to get a string with the user's Identity Provider (e.g.
+    // `https://inrupt.net` or `https://solid.community`) using a method of your choice.
+    async function getIdentityProvider() {
+        
+    }
+
+
+    const identityProvider = await getIdentityProvider();
+
+    /* 3. Initiate the login process - this will redirect the user to their Identity Provider: */
+    auth.login(identityProvider);
+}
