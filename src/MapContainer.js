40a9988106817a95168399855c122ecd12038b03
Fernando <UO266754@uniovi.es>
2020-03-02 12:56:50 +0100

    Changed API key implementation and minor map options.
    This means a Javascript file should be added locally (./src/ApiKey.js) that has a getKey() method that returns the key in a string. This is so the API key is not shared publicly.

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,13 +1,21 @@
 import React from "react";
 import {Map, GoogleApiWrapper} from 'google-maps-react';
 import getStyle from './MapStyles.js';
+import getKey from './ApiKey.js';
+
  
 export class MapContainer extends React.Component {
   render() {
     return (
-      <Map google={this.props.google} zoom={14}
-      
-      styles={getStyle(1)}>
+      <Map
+		  google={this.props.google}
+		  zoom={14}
+		  styles={getStyle(1)}
+		  streetViewControl={false}
+		  mapTypeControl={false}
+		  fullscreenControl={false}
+		  zoomControl={false}
+	  >
                 
       </Map>
     );
@@ -15,6 +23,6 @@ export class MapContainer extends React.Component {
 }
 
 export default GoogleApiWrapper({
-  apiKey: ("AIzaSyBGN3ezdr4GrrSb2XitMq_T6uXSiB6D9uE"),
+  apiKey: (getKey()),
   language:"en-US"
 })(MapContainer)
