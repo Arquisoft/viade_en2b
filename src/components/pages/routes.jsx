a8fa3b057a3fc5073e6a945706cca77e89893a6e
Guillermo Fernández-Campoamor Fernández <uo264207@uniovi.es>
2020-03-04 17:32:14 +0100

    Skeleton of RoutesListView

---
+++ src/components/pages/routes.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import MainNavBar from '../generic_components/MainNavBar'; //Hamburger Menu???
+
+
+const RoutesPage = () => {
+  return (
+    <div className="App">
+      <MainNavBar companyName="VIADE"/>
+      <header className="App-header">
+      </header>
+      <main>
+        <header className="bodyHeader">
+        <header>
+        <body className="bodyRoutes">
+            <form>
+                <input />
+            </form>
+            <section className="sectionRoutes">
+            </section>
+        </body>
+      </main>
+    </div>
+  );
+};
+
+export default RoutesPage;
