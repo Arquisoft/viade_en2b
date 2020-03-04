aadc8530da781b70b89071b6609afe4819ef1baa
Guillermo Fernández-Campoamor Fernández <uo264207@uniovi.es>
2020-03-05 00:54:24 +0100

    Add Some Style

--- src/components/pages/routes.jsx
+++ src/components/pages/routes.jsx
@@ -1,26 +1,43 @@
 import React from 'react';
 
+import '../../assets/css/routes.css';
+
 import MainNavBar from '../generic_components/MainNavBar'; //Hamburger Menu???
 
 
 const RoutesPage = () => {
+
+  var frutas = ["Ruta1", "Ruta2","Ruta3","Ruta4"];
+  
   return (
+    <main>
     <div className="App">
       <MainNavBar companyName="VIADE"/>
-      <header className="App-header">
-      </header>
-      <main>
+      
         <header className="bodyHeader">
-        <header>
+        </header>
         <body className="bodyRoutes">
             <form>
                 <input />
             </form>
             <section className="sectionRoutes">
+              <ul>
+                  {
+	                  frutas.map( (item, index) =>{
+		                return(
+			                  <li key={index}>
+                          <div className="routeListElementContainter">
+				                    <p> {item} nº {index+1} in the list</p>
+                          </div>
+			                  </li>
+		                    )
+	                  })
+                 }
+              </ul>     
             </section>
-        </body>
-      </main>
-    </div>
+        </body> 
+       </div>
+     </main>
   );
 };
 
