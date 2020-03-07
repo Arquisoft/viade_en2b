e51d28bca73d43462fc364dad21cf42fa51b2e9b
Guillermo Fernández-Campoamor Fernández <uo264207@uniovi.es>
2020-03-07 23:57:42 +0100

    Details

--- src/components/pages/routes.jsx
+++ src/components/pages/routes.jsx
@@ -1,47 +1,47 @@
-import React from 'react';
+import React from "react";
 
-import '../../assets/css/routes.css';
+import "../../assets/css/routes.css";
 
-import MainNavBar from '../generic_components/MainNavBar'; //Hamburger Menu???
-
+import MainNavBar from "../generic_components/MainNavBar"; //Hamburger Menu???
 
 const RoutesPage = () => {
+  var frutas = ["Ruta1", "Ruta2", "Ruta3", "Ruta4"];
 
-  var frutas = ["Ruta1", "Ruta2","Ruta3","Ruta4"];
-  
   return (
-    <main>
-    <div className="App">
-      <MainNavBar companyName="VIADE"/>
-      
-        <header className="bodyHeader">
-        </header>
-        <body className="bodyRoutes">
-            
-            <section className="sectionRoutes">
-              <div class="box">
-                <div class="container-2">
-                  <span class="icon"><i class="fa fa-search"></i></span>
-                  <input type="search" id="search" placeholder="Search..." />
-                </div>
-              </div>
-              <ul>
-                  {
-	                  frutas.map( (item, index) =>{
-		                return(
-			                  <li key={index}>
-                          <div className="routeListElementContainter">
-				                    <p> {item} nº {index+1} in the list</p>
-                          </div>
-			                  </li>
-		                    )
-	                  })
-                 }
-              </ul>     
-            </section>
-        </body> 
-       </div>
-     </main>
+    <body className="bodyRoutes">
+      <main>
+        <div className="App">
+        
+
+          <header className="bodyHeader"></header>
+
+          <section className="sectionRoutes">
+            <div class="active-purple-3 active-purple-4 mb-4">
+              <input
+                class="form-control"
+                type="text"
+                placeholder="Search"
+                aria-label="Search"
+              />
+            </div>
+            <ul>
+              {frutas.map((item, index) => {
+                return (
+                  <li key={index}>
+                    <div className="routeListElementContainter">
+                      <p>
+                        {" "}
+                        {item} nº {index + 1} in the list
+                      </p>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </section>
+        </div>
+      </main>
+    </body>
   );
 };
 
