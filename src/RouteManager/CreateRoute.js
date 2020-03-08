c3c79cde299327c0a152a7a0ffe0f4b11e59d9e1
miguel <miguel111max@outlook.es>
2020-03-09 00:50:50 +0100

    Some Fixes

--- src/RouteManager/CreateRoute.js
+++ src/RouteManager/CreateRoute.js
@@ -2,28 +2,55 @@ import Route from "../entities/Route"
 import GeoCoordinate from "../entities/GeoCoordinate"
 import {JsonLdParser} from "jsonld-streaming-parser";
 
-function createRoute(route) {
-    let myParser = new JsonLdParser();
-    myParser
-        .on('data', "test.jsonld")
-        .on('error', console.log("error"));
+class CreateRoute{
+    create(route){
+        let myParser = new JsonLdParser();
+        myParser
+            .on("data", "test.jsonld")
+            .on('error', console.log("Fuck you"));
 
-    myParser.write('{');
-    myParser.write('"@type": "Route",');
-    myParser.write('"name": "route.name",');
-    myParser.write('"itirenary": {');
-    myParser.write('"@type": "Itirenary",');
-    myParser.write('"numberOfitems": "route.itirenary.numberOfItems",');
-    myParser.write('"itemList" : [{');
-    route.itirenary.map(a=>{
-        myParser.write('"@type": "Item",');
-        myParser.write('"order : a.order",');
-        myParser.write('"geoCoordinate: {');
-        myParser.write('"@type": "GeoCoordinate",');
-        myParser.write('"latitude : a.geoCoordinate.latitude",');
-        myParser.write('"longitude : a.geoCoordinate.longitude"');
-        myParser.write('"geoCoordinate: }');
-    })
+        myParser.write('{');
+        myParser.write('"@type": "Route",');
+        myParser.write('"name": "route.name",');
+        myParser.write('"itirenary": {');
+        myParser.write('"@type": "Itirenary",');
+        myParser.write('"numberOfitems": "route.itirenary.numberOfItems",');
+        myParser.write('"itemList" : [{');
+        route.itirenary.map(a => {
+            myParser.write('"@type": "Item",');
+            myParser.write('"order : a.order",');
+            myParser.write('"geoCoordinate: {');
+            myParser.write('"@type": "GeoCoordinate",');
+            myParser.write('"latitude : a.geoCoordinate.latitude",');
+            myParser.write('"longitude : a.geoCoordinate.longitude"');
+            myParser.write('}');
+        })
 
-    myParser.write(']}');
+        myParser.write(']}');
+    }
+
+    createNormal(route){
+        let str = "{";
+        str+="@type: Route,";
+        str+="name : '"+route.name+"',";
+        str+="itirenary: {";
+        str+="@type: 'Itirenary',";
+        str+="numberOfitems : '"+route.itirenary.numberOfItems+"',";
+        str+="itemList : [{";
+        route.itirenary.map(a =>{
+            str+= "@type: 'Item',";
+            str+= "order : '" +a.order +"',";
+            str+= "geoCoordinate : {"
+            str+= "latitude : '" +a.geoCoordinate.latitude+"',";
+            str+= "longitude : '" +a.geoCoordinate.longitude+"'";
+            str+="}"
+        })
+        str+= "}]}";
+
+        let json = JSON.stringify(eval(str));
+        let fs = require('fs');
+        fs.writeFile("test.json", json,err => console.log("something went wrong"));
+    }
 }
+
+export default CreateRoute;
--- src/testing/createSomeRoutes.js
+++ src/testing/createSomeRoutes.js
@@ -1,20 +1,28 @@
+
 import Route from "../entities/Route"
 import GeoCoordinate from "../entities/GeoCoordinate"
 import Item from "../entities/Item"
 import Itirenary from "../entities/Itirenary"
 import CreateRoute from "../RouteManager/CreateRoute.js"
-const list = new GeoCoordinate[10];
-let i = 0;
-for(i = 0; i<10; i++){
-    let g = new GeoCoordinate(i, i+1);
-    list.add(g);
-}
-const items = new Item[10];
-for(i = 0; i<10; i++){
-    let g = new Item(GeoCoordinate, i);
-    items.add(g);
+
+function createSomeRoutes() {
+    const list = new GeoCoordinate[10];
+    let i = 0;
+    for(i = 0; i<10; i++){
+        let g = new GeoCoordinate(i, i+1);
+        list.add(g);
+    }
+    const items = new Item[10];
+    for(i = 0; i<10; i++){
+        let g = new Item(GeoCoordinate, i);
+        items.add(g);
+    }
+
+    const itirenary = new Itirenary(10, items);
+    const route = new Route("test", itirenary);
+
+    const routecreator = new CreateRoute();
+    routecreator.create(route);
 }
 
-const itirenary = new Itirenary(10, items);
-const route = new Route("test", itirenary);
-
+export default createSomeRoutes;
