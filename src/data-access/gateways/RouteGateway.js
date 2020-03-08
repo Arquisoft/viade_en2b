604861c73974846ea189d29491226a78dd069aba
uo264345 <uo264345@uniovi.es>
2020-03-08 20:31:06 +0100

    Gateway treated as a module
    - Now the gateway can be imported into other classes to call the functions.
    - Typescript gateway deleted. Not needed for now.

--- src/data-access/gateways/RouteGateway.js
+++ src/data-access/gateways/RouteGateway.js
@@ -1,39 +1,38 @@
-class RouteGateway {
-    async findByName(name) {
-        // call to ldflex to find :Route with subject #name
-        
+export default class RouteGateway {
+    findByName(name) {
     }
 
-    async findAll() {
+    findAll() {
         // call to ldflex to find every :Route
-        var routes = RouteRDF.findAll();
+        //var routes = RouteRDF.findAll();
         // convert the routes to JSON
-        var parsedRoutes = RouteJSONtoRDF.toJSON(routes);
+        //var parsedRoutes = RouteJSONtoRDF.toJSON(routes);
+        //return parsedRoutes;
     }
 
-    async add(route) {
+    add(route) {
         // Note: route is in GeoJSON format, or similar
         // pass the route to RouteJSONtoRDF, receive RDF object
-        var parsedRoute = RouteJSONtoRDF.toRDF(route);
+        //var parsedRoute = RouteJSONtoRDF.toRDF(route);
         // call to add the route in the pod accordingly
-        await RouteRDF.add(parsedRoute);
+        //await RouteRDF.add(parsedRoute);
     }
 
-    async deleteByName(name) {
+    deleteByName(name) {
         // call to find the route by the name
-        var route = await RouteRDF.findByName(name);
+        //var route = await RouteRDF.findByName(name);
         // if any, call again to delete it
-        if(route && route.name) {
-            RouteRDF.delete(route);
-        }
+        //if(route && route.name) {
+        //    RouteRDF.delete(route);
+        //}
     }
 
-    async updateByName(name, route) {
+    updateByName(name, route) {
         // call to ldflex to find route by the name
-        var route = await RouteRDF.findByName(name);
+        //var route = await RouteRDF.findByName(name);
         // if any, modify it
-        if(route && route.name) {
-            RouteRDF.update(route);
-        }
+        //if(route && route.name) {
+        //    RouteRDF.update(route);
+        //}
     }
 }
