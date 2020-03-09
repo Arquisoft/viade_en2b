d554dfdf3a3814b42e68d5c336660bb1de7b6b61
Violeta <uo258454@uniovi.es>
2020-03-09 17:54:19 +0100

    Fixed all package.json + restarted listing routes

--- src/ListUserRoutes.js
+++ src/ListUserRoutes.js
@@ -1,21 +1,14 @@
 const auth = require('solid-auth-client')
 const FC   = require('solid-file-client')
-const fc   = new FC(  auth)
-
-
-
-async function getRoutes(){
-   let session = await auth.currentSession()
+const fc   = new FC( auth )
+async function run(){
+    let session = await auth.currentSession()
     if (!session) { session = await auth.login() }
+    console.log(`Logged in as ${session.webId}.`)
+    if( await fc.itemExists( session.webId+"/public/routes") ){
 
-    getWebId("https://migi.solid.community/");
-    //console.log(`Logged in as ${session.webId}.`)
-    if( await fc.itemExists( "https://violetaruizm.inrupt.net/Public/Routes" ) ){
-        let content = await fc.readFile( "https://violetaruizm.inrupt.net/Public/Routes")
-       console.log("Routes directory exists");
-        console.log(content);
+        // ... other file methods
+        // ... and/or other auth methods
     }
-
 }
-
-getRoutes();
+run()
