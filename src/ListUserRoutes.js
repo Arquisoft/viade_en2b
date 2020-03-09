6ecd96f001efa16e32ba6a1ae3f14873a66b4b9f
Violeta <uo258454@uniovi.es>
2020-03-09 12:13:30 +0100

    Added getting webid from session

--- src/ListUserRoutes.js
+++ src/ListUserRoutes.js
@@ -2,9 +2,13 @@ const auth = require('solid-auth-client')
 const FC   = require('solid-file-client')
 const fc   = new FC(  auth)
 
+
+
 async function getRoutes(){
-   //let session = await auth.currentSession()
-    //if (!session) { session = await auth.login() }
+   let session = await auth.currentSession()
+    if (!session) { session = await auth.login() }
+
+    getWebId("https://migi.solid.community/");
     //console.log(`Logged in as ${session.webId}.`)
     if( await fc.itemExists( "https://violetaruizm.inrupt.net/Public/Routes" ) ){
         let content = await fc.readFile( "https://violetaruizm.inrupt.net/Public/Routes")
