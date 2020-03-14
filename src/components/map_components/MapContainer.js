ef7568cd00be6b4f921124b51ae5d6f8b599eaca
fernandossmm <UO266754@uniovi.es>
2020-03-14 03:26:17 +0100

    Route displayed enhanced.
    A marker is shown at the start of the route.
    The map centers around the mid point of the route.
    A locally defined dummy route has been used for testing.

--- src/components/map_components/MapContainer.js
+++ src/components/map_components/MapContainer.js
@@ -1,23 +1,46 @@
 import React from "react";
-import {Map, GoogleApiWrapper} from 'google-maps-react';
+import {Map, Polyline, Marker, InfoWindow, GoogleApiWrapper} from 'google-maps-react';
 import getStyle from './MapStyles.js';
 import getKey from 'ApiKey.js';
 
  
 export class MapContainer extends React.Component {
+  center={lat: 43.362448, lng: -5.849005}
+  route = [{lat: 43.361778, lng: -5.848008}, {lat: 43.363836, lng: -5.851059}, {lat: 43.363174, lng: -5.852273}]
+  zoom = 15
+  
+  setZoom(zoom) { this.zoom = zoom }
+  
   render() {
-    return (
-      <Map
-		  google={this.props.google}
-		  zoom={14}
-		  styles={getStyle(1)}
-		  streetViewControl={false}
-		  mapTypeControl={false}
-		  fullscreenControl={false}
-		  zoomControl={false}
-	  >
-                
-      </Map>
+    if(this.route.length!==0) {
+      this.center=this.route[Math.floor(this.route.length/2)];
+    }
+    
+    return ( <Map
+      google={this.props.google}
+      zoom={this.zoom}
+      styles={getStyle(0)}
+      streetViewControl={false}
+      mapTypeControl={false}
+      fullscreenControl={false}
+      zoomControl={false}
+      initialCenter={this.center}
+    >
+    <Polyline
+      path={this.route}
+      strokeColor="#717171"
+      strokeOpacity={1}
+      strokeWeight={0.4*this.zoom}
+      
+      />
+      <Marker
+        title={'Start of route'}
+        name={'Start of route'}
+        position={this.route[0]}
+        opacity={1}
+        icon={"https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Media_Viewer_Icon_-_Location.svg/35px-Media_Viewer_Icon_-_Location.svg.png"}
+      />
+    </Map>
     );
   }
 }
