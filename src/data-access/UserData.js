6f4c7e3c5bf9571cba895a09e4f8d7fac2b2a689
Clara <UO264958@uniovi.es>
2020-03-18 09:57:37 +0100

    Trying to fix @solid/query-ldflex import problem

--- src/data-access/UserData.js
+++ src/data-access/UserData.js
@@ -1,10 +1,29 @@
 import { useLDflexValue, useLDflexList } from '@solid/react';
-
+import data from '@solid/query-ldflex';
 
 export function GetUserName() {
     const name = useLDflexValue('user.name') || 'unknown';
     return name.value;
 }
+
+export async function GetUserProfileImage() {
+    try {
+      // fetching user card from pod. This makes a request and returns the data
+     
+      const { user } = data;
+      /*
+       * In the background LDFlex is using JSON-LD. Because of this, we need to
+       * make an async call. This will return a JSON-LD expanded object and expose the requested value(name).
+       * for more information please go to: https://github.com/digitalbazaar/jsonld.js
+       */
+      const userImage = data.user.vcard_hasPhoto;
+      const { image: defaultimage } = this.state;
+      const image = userImage ? userImage.value : defaultimage;
+      this.setState({ image });
+    } catch (error) {
+      alert(error.message);
+    }
+  };
 
 
 export function GetUserFriends(){
