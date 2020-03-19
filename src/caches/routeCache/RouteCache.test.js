e11648f6846e08c3d70b64a8f9b256c5d912e094
uo264345 <uo264345@uniovi.es>
2020-03-19 17:23:38 +0100

    Treat RouteCache as a single instance
    - Only a single object is exported.
    - Tests passed.

--- src/caches/routeCache/RouteCache.test.js
+++ src/caches/routeCache/RouteCache.test.js
@@ -41,6 +41,7 @@ var dummyRouteOnlyName = {
 };
 
 beforeEach(() => {
+    RouteCache.routes = []; //Clear the routes saved
     mockGatewayAdd.mockClear();
     mockGatewayFindAll.mockClear();
     mockGatewayFind.mockClear();
@@ -48,67 +49,59 @@ beforeEach(() => {
 });
 
 test('add new route', () => {
-    let cache = new RouteCache();
-    cache.addRoute(dummyRoute1);
+    RouteCache.addRoute(dummyRoute1);
 
     expect(mockGatewayAdd).toHaveBeenCalled();
 });
 
 test('add cached route', () => {
-    let cache = new RouteCache();
-    cache.addRoute(dummyRoute1);
-    cache.addRoute(dummyRouteRepeated);
+    RouteCache.addRoute(dummyRoute1);
+    RouteCache.addRoute(dummyRouteRepeated);
 
     expect(mockGatewayAdd).toHaveBeenCalledTimes(1);
 });
 
 test('get all the routes first time', () => {
-    let cache = new RouteCache();
-    cache.getRoutes();
+    RouteCache.getRoutes();
 
     expect(mockGatewayFindAll).toHaveBeenCalled();
 });
 
 test('get all the routes when having them cached', () => {
-    let cache = new RouteCache();
-    cache.addRoute(dummyRoute1);
-    cache.getRoutes();
+    RouteCache.addRoute(dummyRoute1);
+    RouteCache.getRoutes();
 
     expect(mockGatewayFindAll).not.toHaveBeenCalled();
 });
 
 test('get a route in the cache', () => {
-    let cache = new RouteCache();
-    cache.addRoute(dummyRoute1);
-    let cachedRoute = cache.getSelected(dummyRouteOnlyName);
+    RouteCache.addRoute(dummyRoute1);
+    let cachedRoute = RouteCache.getSelected(dummyRouteOnlyName);
 
     expect(mockGatewayFind).not.toHaveBeenCalled();
     expect(cachedRoute).toEqual(dummyRoute1);
 });
 
 test('get a route not in the cache', () => {
-    let cache = new RouteCache();
-    cache.getSelected(dummyRoute1);
+    RouteCache.getSelected(dummyRoute1);
 
     expect(mockGatewayFind).toHaveBeenCalled();
 });
 
 test('delete a route in the cache', () => {
-    let cache = new RouteCache();
-    cache.addRoute(dummyRoute1);
-    cache.addRoute(dummyRoute2);
-    cache.deleteRoute(dummyRoute2);
+    RouteCache.addRoute(dummyRoute1);
+    RouteCache.addRoute(dummyRoute2);
+    RouteCache.deleteRoute(dummyRoute2);
 
     expect(mockGatewayDelete).toHaveBeenCalled();
-    expect(cache.routes.length).toBe(1);
+    expect(RouteCache.routes.length).toBe(1);
 });
 
 test('delete a route not in the cache', () => {
-    let cache = new RouteCache();
-    cache.addRoute(dummyRoute1);
-    cache.addRoute(dummyRoute2);
-    cache.deleteRoute(dummyRoute3);
+    RouteCache.addRoute(dummyRoute1);
+    RouteCache.addRoute(dummyRoute2);
+    RouteCache.deleteRoute(dummyRoute3);
 
     expect(mockGatewayDelete).toHaveBeenCalled();
-    expect(cache.routes.length).toBe(2);
+    expect(RouteCache.routes.length).toBe(2);
 });
