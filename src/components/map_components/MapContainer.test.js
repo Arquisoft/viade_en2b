651437bd1050d6f452fad4f305a4dd4b3641fc43
miguel <miguel111max@outlook.es>
2020-03-19 01:11:35 +0100

    Tests Map view First Draft

---
+++ src/components/map_components/MapContainer.test.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import MapContainer from "./MapContainer";
+import Adapter from 'enzyme-adapter-react-16';
+import Enzyme, { shallow, mount } from 'enzyme';
+import {expects} from "rdf-namespaces/dist/hydra";
+
+
+
+Enzyme.configure({ adapter: new Adapter() });
+
+test('isDefined', ()=>{
+    expect(<MapContainer/>).toBeDefined();
+});
+
+
+//ToChange
+/*
+test('TestExistance of button', () => {
+    const mockLogout = jest.fn();
+    const button = <GenericButton onClick={mockLogout}/>
+    const wrapper = shallow(button);
+
+    wrapper.simulate('click');
+    expect(mockLogout).toHaveBeenCalled();
+});
+*/
---
+++ src/components/map_components/MapRoute.test.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import MapRoute from "./MapContainer";
+import Adapter from 'enzyme-adapter-react-16';
+import Enzyme, { shallow, mount } from 'enzyme';
+import {expects} from "rdf-namespaces/dist/hydra";
+
+
+test('isDefined', ()=>{
+    expect(<MapRoute/>).toBeDefined();
+});
