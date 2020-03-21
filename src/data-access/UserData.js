bffdbf7728f5a01e37327e212d405d1350f344f2
Clara <UO264958@uniovi.es>
2020-03-21 18:38:05 +0100

    Added GetUserProfileImage method
    Retrieves the image of the user

--- src/data-access/UserData.js
+++ src/data-access/UserData.js
@@ -6,46 +6,12 @@ export function GetUserName() {
   return name.value;
 }
 
-export async function GetUserProfileImage() {
-  
-    
+export async function GetUserProfileImage(webId) {
+  return webId.vcard_hasPhoto;    
 };
-
 
 export function GetUserFriends() {
   const friends = useLDflexList('user.friends');
-  return friends;
-}
-
-export async function GetUserFriendsData() {
-
-  let friends = [];
-  const userFriends = GetUserFriends();
-
-  for await (const friend of userFriends) {
-
-    const friendWebId = await friend.value;
-    const friend_data = data[friendWebId];
-    const nameLd = await friend_data.name;
-
-    const name = nameLd && nameLd.value.trim().length > 0 ? nameLd.value : friendWebId.toString();
-    const imageLd = await friend_data.vcard_hasPhoto;
-
-    let image;
-    if (imageLd && imageLd.value) {
-      image = imageLd.value;
-    } else {
-      image = "";
-    }
-
-    var friend_obj = {
-      "webId": friendWebId,
-      "name": name,
-      "image": image
-    };
-    friends.push(friend_obj);
-  }
-
   return friends;
 }
 
