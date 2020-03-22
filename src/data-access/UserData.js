b06dac12beaa8bdc79216e469427b2ef662c3dbb
Clara <UO264958@uniovi.es>
2020-03-22 18:34:51 +0100

    Friends data retrieval added

--- src/data-access/UserData.js
+++ src/data-access/UserData.js
@@ -1,18 +1,55 @@
 import { useLDflexValue, useLDflexList } from '@solid/react';
-import data from '@solid/query-ldflex';
+
+const { default: data } = require('@solid/query-ldflex');
+
+
 
 export function GetUserName() {
   const name = useLDflexValue('user.name') || 'unknown';
   return name.value;
 }
 
+export async function GetSepcificName(webId) {
+  const personName = await webId.name;
+  try{
+    const toReturn = personName.value;
+    return toReturn;
+  } catch (TypeError ){
+    return console.log("There was some kind of problem");
+  }
+}
+
+
 export async function GetUserProfileImage(webId) {
-  return webId.vcard_hasPhoto;    
+  const photo = useLDflexValue('user.vcard_hasPhoto') || 'unknown';
+  return photo.value;    
 };
 
-export function GetUserFriends() {
+export async function GetUserFriends() {
   const friends = useLDflexList('user.friends');
-  return friends;
+  
+  let friendsArray = [];
+  let friendsNamesArray = [];
+  
+  console.log('HEY');
+  
+  console.log(friends.length);
+  for(let i =0; i<i.length; i++){
+
+  }
+  friends.forEach(async friendLDflexValue =>{
+    
+    let friendWebId = friendLDflexValue.value;
+    friendsArray.push(friendWebId);
+
+    const ruben = data[friendWebId];
+    console.log('FRIEND:'+ruben);
+    console.log('NAME:'+ await GetSepcificName(ruben));
+    friendsNamesArray.push('NAME:'+ await GetSepcificName(ruben));
+  });
+  
+
+  return friendsNamesArray;
 }
 
 export async function GetUserWebId() {
@@ -26,3 +63,4 @@ export function GetNumberOfFriends() {
   const friends = useLDflexList('user.friends');
   return friends.length;
 }
+
