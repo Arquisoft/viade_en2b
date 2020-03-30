68ac429644fc72a409d1ac93f7f277ce92ad46f4
Clara <UO264958@uniovi.es>
2020-03-30 17:11:24 +0200

    Tried to add a check in the delete user method

--- src/FriendManager/DeleteFriend.js
+++ src/FriendManager/DeleteFriend.js
@@ -1,13 +1,39 @@
 import ldflex from '@solid/query-ldflex';
-import {GetUserWebId} from '../data-access/UserData';
+import {GetUserWebId} from '../data-access/UserData'
+import Friend from '../Entities/Friend';
 
-export async function DeleteFriend(webId){
+export async function DeleteFriend(webId, friends){
     console.log('User:');
     console.log(await GetUserWebId());  
     console.log('Wants to delete user:');  
     console.log(webId);
 
 
-    console.log('DELETING');
-    return ldflex[await GetUserWebId()].knows.delete(ldflex[webId]); 
+    let friendsAux = [];
+    friendsAux= asyncFunc(friends);    
+
+       
+    console.log('AFTER');
+    console.log('ARRAY');
+    console.log(friendsAux);
+
+    
+    
+    friendsAux.forEach(async friend => {
+        
+        const webIdFriend = friend.webId;
+        console.log(webIdFriend);
+
+        if(webIdFriend == webId){
+            console.log('CAN BE DELETED');
+            return ldflex[await GetUserWebId()].knows.delete(ldflex[webId]);
+        }
+    });
+
+    console.log('COULD NOT BE DELETED');   
 }
+
+async function asyncFunc(friends){
+    
+}
+
