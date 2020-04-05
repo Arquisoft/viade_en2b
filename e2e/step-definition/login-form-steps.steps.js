84a693259fabad09f337b4d6d2f376129f6abfe3
Guillermo Fernández-Campoamor Fernández <uo264207@uniovi.es>
2020-04-05 19:08:41 +0200

    Login Cucumber Test

---
+++ e2e/step-definition/login-form-steps.steps.js
@@ -0,0 +1,29 @@
+const {defineFeature,loadFeature}=require('jest-cucumber');
+const feature = loadFeature('e2e/features/test.feature');
+
+defineFeature(feature,test=>{
+
+    beforeEach(async () =>{
+        await page.goto('/')
+    });
+
+    test('The user is not logged in the application',({given,when,then})=>
+    {
+
+
+        given('Given a logout user',()=>{
+            page.goto('/login')     
+
+    })
+
+
+        when('When the user has reach the page',()=>{
+            await expect(page).toClick('Login',{text : 'Log In'})
+
+    });
+
+        then('User is logged',()=>{
+            console.log("User logged");
+        })
+    })
+})
