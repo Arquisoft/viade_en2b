529a1f5ac5fc63a893b42243e42ebc666fcd2a63
Guillermo Fernández-Campoamor Fernández <uo264207@uniovi.es>
2020-04-06 17:21:51 +0200

    Update login-form-steps.steps.js

--- e2e/step-definition/login-form-steps.steps.js
+++ e2e/step-definition/login-form-steps.steps.js
@@ -4,7 +4,7 @@ const feature = loadFeature('e2e/features/test.feature');
 defineFeature(feature,test=>{
 
     beforeEach(async () =>{
-        await page.goto('/')
+        await page.goto('https://arquisoft.github.io/viade_en2b/#/')
     });
 
     test('The user is not logged in the application',({given,when,then})=>
@@ -12,7 +12,7 @@ defineFeature(feature,test=>{
 
 
         given('Given a logout user',()=>{
-            page.goto('/login')     
+            page.goto('https://arquisoft.github.io/viade_en2b/#/login')     
 
     })
 
