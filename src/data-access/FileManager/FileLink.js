75554139aeefd0bcb9e2b08f3f3eebbb714fbe11
uo264345 <uo264345@uniovi.es>
2020-04-14 22:32:30 +0200

    Fixed support for JSONLD files.

--- src/data-access/FileManager/FileLink.js
+++ src/data-access/FileManager/FileLink.js
@@ -1,5 +1,6 @@
 import * as auth from "solid-auth-client";
 import SolidFileClient from "solid-file-client";
+import { fetchDocument } from "tripledoc";
 
 import { handleFetchError } from "./FileUtils";
 
@@ -13,24 +14,37 @@ export const linkFilesToRoute = async (fileUris, routeName) => {
   let fileClient = new SolidFileClient(auth, { enableLogging: true });
   let session = await auth.currentSession();
   let storageRoot = session.webId.split("profile")[0];
-  let buildRoutePath = storageRoot + routesFolder + routeName + ".json";
+  let buildRouteFolderPath = storageRoot + routesFolder;
   let attachementDate = getAttachmentDate();
-  if (await fileClient.itemExists(buildRoutePath)) {
-    // get the route
-    let routeFile = await fileClient.readFile(buildRoutePath);
-    let route = JSON.parse(routeFile);
-    // check the media subject
-    if (!route.media) {
-      route.media = [];
-    }
-    // for each file
-    // add a new ref to a media subject
-    fileUris.forEach((fileUri) => {
-      route.media.push({ "@id": fileUri, dateTime: attachementDate });
+  if (await fileClient.itemExists(buildRouteFolderPath)) {
+    let viadeRoutes = await fc.readFolder(storageRoot + routesFolder);
+    let routeFiles = viadeRoutes.files;
+
+    routeFiles.forEach(file => {
+      if(file.url.match(new RegExp(routeName + "\.json"))) {
+        let routeFile = await fileClient.readFile(file.url);
+        let route = JSON.parse(routeFile);
+        if (!route.media) {
+          route.media = [];
+        }
+
+        fileUris.forEach((fileUri) => {
+          route.media.push({ "@id": fileUri, dateTime: attachementDate });
+        });
+        fileClient
+          .putFile(file.url, JSON.stringify(route), file.type)
+          .catch(handleFetchError);
+      } else if(file.url.match(new RegExp(routeName + "\.ttl"))) {
+        let routeDocument = await fetchDocument(file.url);
+      }
     });
-    // save the updated route
-    fileClient
-      .putFile(buildRoutePath, JSON.stringify(route), "application/json")
-      .catch(handleFetchError);
   }
 };
+
+const treatJSONRoute = (routeFile) => {
+
+}
+
+const treatTurtleRoute = (routeFile) => {
+  
+}
