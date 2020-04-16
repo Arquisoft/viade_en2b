1ae7856ad873bf4e7d50c1517757b682ee47f8e3
Guillermo Fernández-Campoamor Fernández <uo264207@uniovi.es>
2020-04-16 12:34:45 +0200

    Main Functionality Of Show User Image and Name
    * Need a default photo is user don´t have an image
    * CSS need

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,45 +1,125 @@
-import React from 'react';
-import { LoggedIn, LoggedOut} from '@solid/react';
-import cache from '../../caches/routeCache/RouteCache'
-import * as friendCache from 'caches/friendCache/FriendCache';
+import React from "react";
 
-function Login() {
-    const auth = require('solid-auth-client');
-    function logout(e, auth) {
-        e.preventDefault();
-        auth.logout();
-        cache.clear();
-        friendCache.default.clear();
-    }
+import { LoggedIn, LoggedOut } from "@solid/react";
+import cache from "../../caches/routeCache/RouteCache";
+import * as friendCache from "caches/friendCache/FriendCache";
+import data from "@solid/query-ldflex";
 
-    return (
-        <div>
-            <LoggedOut>
-                <div className="wrap-input100">
-                    <input id="inputLogin" className="input100" list="providers" type="text" name="provider" placeholder="Provider" />
-                    <datalist id="providers">
-                        <option value="https://solid.community/" />
-                        <option value="https://inrupt.net/" />
-                    </datalist>
-                    <span className="focus-input100"></span>
-                </div>
-                <button className="login100-form-btn" onClick={(e) => popup(e, auth)}>Log In</button>
-            </LoggedOut>
-            <LoggedIn>
-               <button className="login100-form-btn" onClick={(e) => logout(e, auth)}>Log out</button>                
-            </LoggedIn>
+import * as userprofile from "../../data-access/UserData";
 
-        </div>
-    )
+const $rdf = require("rdflib");
+const auth = require("solid-auth-client");
+
+function logout(e, auth) {
+  e.preventDefault();
+  auth.logout();
+  cache.clear();
+  friendCache.default.clear();
 }
 
-async function popup(e, auth) {
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: "",
+      name: "",
+    };
+  }
+  componentDidMount() {
+    this.getProfileImage().then((foto) => {
+      this.setState({ image: foto });
+    });
+    this.getProfileName().then((name) => {
+      this.setState({ name: name });
+    });
+  }
+
+  componentDidUpdate() {
+    this.getProfileImage().then((foto) => {
+      this.setState({ image: foto });
+    });
+    this.getProfileName().then((name) => {
+      this.setState({ name: name });
+    });
+  }
+
+  async popup(e, auth) {
     e.preventDefault();
     let session = await auth.currentSession();
-   let popupUri = 'https://solid.community/common/popup.html';
-    if (!session)
-        session = await auth.popupLogin({ popupUri });
+    let popupUri = "https://solid.community/common/popup.html";
+    if (!session) session = await auth.popupLogin({ popupUri });
+    this.getProfileImage().then((foto) => {
+      this.setState({ image: foto });
+    });
+    this.getProfileName().then((name) => {
+      this.setState({ name: name });
+    });
+    auth.fetch(session).then(console.log);
     alert(`Logged in as ${session.webId}`);
+  }
+
+  async getProfileImage() {
+    try {
+      const { user } = data;
+      const userImage = await user.vcard_hasPhoto;
+      return userImage.value;
+    } catch (error) {
+      //errorToaster(error.message, 'Error');
+    }
+  }
+
+  async getProfileName() {
+    try {
+      const { user } = data;
+      const userName = await user.name;
+      return userName.value;
+    } catch (error) {
+      //errorToaster(error.message, 'Error');
+    }
+  }
+
+  viewImageLoaded = () => {};
+  render() {
+    const { image } = this.state;
+
+    return (
+      <div>
+        <LoggedOut>
+          <div className="wrap-input100">
+            <input
+              id="inputLogin"
+              className="input100"
+              list="providers"
+              type="text"
+              name="provider"
+              placeholder="Provider"
+            />
+            <datalist id="providers">
+              <option value="https://solid.community/" />
+              <option value="https://inrupt.net/" />
+            </datalist>
+            <span className="focus-input100"></span>
+          </div>
+          <button
+            className="login100-form-btn"
+            onClick={(e) => this.popup(e, auth)}
+          >
+            Log In
+          </button>
+        </LoggedOut>
+        <LoggedIn>
+          <img src={this.state.image} />
+          <p>{this.state.name}</p>
+          <button
+            className="login100-form-btn"
+            onClick={(e) => logout(e, auth)}
+          >
+            Log out
+          </button>
+        </LoggedIn>
+      </div>
+    );
+  }
 }
 
 export default Login;
