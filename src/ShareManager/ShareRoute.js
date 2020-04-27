b4c7dc9a850fef0fdfe3a9a226257ec50d679854
Clara <UO264958@uniovi.es>
2020-04-27 23:33:51 +0200

    Fixed paths in share route

--- src/ShareManager/ShareRoute.js
+++ src/ShareManager/ShareRoute.js
@@ -12,30 +12,38 @@ import { v4 as uuidv4 } from "uuid";
  * and sends a notification to the inbox of the friend, containing the url of that
  * route.
  *
- * Example: ShareWith( "https://clrmrnd.inrupt.net/viade/routes/Rusia.json", "https://testingclrmrnd.inrupt.net/", "https://clrmrnd.inrupt.net/")
+ * Example: ShareWith( "https://clrmrnd.inrupt.net/viade/routes/Rusia.json", "https://testingclrmrnd.inrupt.net/profile/card#me", "https://clrmrnd.inrupt.net/profile/card#me")
  *
  * @param {String} route path to the route the user wants to share.
- * @param {String} webIdFriend represents the id of the friend.
- * @param {String} webIdAuthor represents the id of the user autenticated.
+ * @param {String} profileFriend represents profile card of the friend. 
+ * @param {String} profileAuthor represents the profile card of the user autenticated.
  */
-export async function ShareWith(route, webIdFriend, webIdAuthor) {
+export async function ShareWith(route, profileFriend, profileAuthor) {
   //Sharing the route with the friend (it must be the profile of the friend).
   console.log("Route: " + route);
-  console.log("Friend ID: " + webIdFriend);
-  console.log("Author ID: " + webIdAuthor);
-  webIdAuthor = webIdAuthor.substring(0, webIdAuthor.length - 16);
-  //const profileFriend = webIdFriend + "profile/card#me";
-  const profileFriend = webIdFriend.substring(0, webIdFriend.length - 16);
+  console.log("Friend ID: " + profileFriend);
+  console.log("Author ID: " + profileAuthor);
+
+  let webIdAuthor = profileAuthor.substring(0, profileAuthor.length - 16);
+  webIdAuthor = webIdAuthor + "/";
+
+  
+  let webIdFriend = profileFriend.substring(0, profileFriend.length - 16);
+  webIdFriend = webIdFriend + "/";
+
   console.log("FRIEND");
-  console.log(profileFriend);
+  console.log(webIdFriend);
+  console.log(webIdAuthor);
 
   //check .acl created for the path;
 
   //check friend has an inbox;
 
-  //check if it's already shared
+  //check if it's already shared (you have to check and set permissions to the /profile/card#me)
+
   const shared = await checkPermissions("READ", profileFriend, route);
   if (!shared) {
+
     //set permissions to read in the route
     setPermissionsTo("READ", route, profileFriend);
 
