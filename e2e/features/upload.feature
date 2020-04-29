39434810cd0f3fb7821fcaa99416f1f4c573f5c7
uo264345 <uo264345@uniovi.es>
2020-04-29 21:03:09 +0200

    Fixing bugs in upload feature test

--- e2e/features/upload.feature
+++ e2e/features/upload.feature
@@ -1,12 +1,6 @@
-Feature: 
-  As a user of the application
-  I want to upload a file to a route
-  So that me and the people I shared the route with can see it attached to that specific route
+Feature: Upload a file
 
-  Scenario: Upload a file and link it to a route
+  Scenario: Upload an image and link it to a route
     Given A logged in user in the routes list page
-    When The user selects a route
-        And Uploads a file
-    Then The user goes to the routes list page
-        And views the files attached to the route
-        And finds the just-uploaded file
+    When The user selects a route and uploads a file
+    Then The user goes to the routes list page and finds the just-uploaded file attached to the route
--- e2e/step-definitions/upload.steps.js
+++ e2e/step-definitions/upload.steps.js
@@ -7,54 +7,60 @@ defineFeature(feature, (test) => {
   let user = "viadeen2bpod";
   let pass = "ASW1920en2b$";
 
-  beforeAll(async () => {
-    await page.goto("http://localhost:3000/viade_en2b/#/login", {
-      waitUntil: "load",
-      timeout: 0,
-    });
-    await page.waitFor(12000);
-    await expect(page).toClick("button", { text: "Log In" });
-    const newPagePromise = new Promise((x) =>
-      browser.once("targetcreated", (target) => x(target.page()))
-    );
-    const popup = await newPagePromise;
-    await popup.waitFor(2000);
-    await expect(popup).toClick("button", { text: "Solid Community" });
-    await expect(popup).toClick("button", { text: "Go" });
-    await popup.waitFor(5000);
-    await expect(popup).toFillForm("form", {
-      username: user,
-      password: pass,
-    });
-    await expect(popup).toClick("button", { text: "Log In" });
-    await page.waitFor(5000);
-    await expect(page).toMatchElement("button", {
-      text: "Log out",
-    });
+  beforeEach(async () => {
+    await page.goto("http://localhost:3000", { waitUntil: "load", timeout: 0 });
   });
 
-  beforeEach(async () => {
+  afterEach(async () => {
     await page.goto("http://localhost:3000/viade_en2b/#", {
       waitUntil: "load",
       timeout: 0,
     });
   });
 
-  test("The user wants to log into application", ({ given, when, then }) => {
+  test("Upload an image and link it to a route", ({ given, when, then }) => {
     given("A logged in user in the routes list page", async () => {
+      // login sequence
+      await page.waitFor(10000);
       await expect(page).toClick("button", { text: "Open Menu" });
       await page.waitFor(1000);
       await expect(page).toClick("[href='#/routes']");
-      await page.waitForNavigation({ waitUntil: "networkidle0" });
+      await page.waitFor(1000);
+      await expect(page).toClick("button", { text: "Log In" });
+      const newPagePromise = new Promise((x) =>
+        browser.once("targetcreated", (target) => x(target.page()))
+      );
+      const popup = await newPagePromise;
+      await popup.waitFor(5000);
+      await expect(popup).toClick("button", { text: "Solid Community" });
+      await expect(popup).toClick("button", { text: "Go" });
+      await popup.waitFor(10000);
+      await expect(popup).toFillForm("form", {
+        username: user,
+        password: pass,
+      });
+      await expect(popup).toClick("button", { text: "Log In" });
+      await page.waitFor(10000);
+      await expect(page).toMatchElement("button", {
+        text: "Log out",
+      });
+      await page.waitFor(2000);
+      await expect(page).toClick("button", { text: "Open Menu" });
+      await page.waitFor(1000);
+      await expect(page).toClick("[href='#/routes']");
+      await page.waitFor("i[class='map icon']");
     });
 
     when("The user selects a route and uploads a file", async () => {
+      await page.waitFor(2000);
       await expect(page).toClick("[href='#/']");
-      await page.waitFor(1000);
+      await page.waitFor(5000);
+      await jestPuppeteer.debug();
+      await page.waitFor("button[class='rtf--mb']");
       await expect(page).toClick("button", { class: "rtf--mb" });
-      await page.waitFor(500);
+      await page.waitFor(1000);
       await expect(page).toClick("button", { text: "Take Photo" });
-      await page.waitFor(500);
+      await page.waitFor(1000);
       await expect(page).toUploadFile(
         "input[type='file']",
         path.join("../../public/images", "Viade.png")
@@ -63,12 +69,12 @@ defineFeature(feature, (test) => {
     });
 
     then(
-      "The user goes to the routes list page and views the files attached to the route and finds the just-uploaded file",
+      "The user goes to the routes list page and finds the just-uploaded file attached to the route",
       async () => {
         await expect(page).toClick("button", { text: "Open Menu" });
         await page.waitFor(1000);
         await expect(page).toClick("[href='#/routes']");
-        await page.waitForNavigation({ waitUntil: "networkidle0" });
+        await page.waitFor("i[class='info icon']");
         await expect(page).toClick("[href='#/routes']");
         await page.waitFor(1000);
         await expect(page).toClick("a", { text: "Imagenes" });
