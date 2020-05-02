f3eb480e677e2d840168c4a6f74086d7f7614f46
Guillermo Fernández-Campoamor Fernández <uo264207@uniovi.es>
2020-05-02 03:33:08 +0200

    Update GetComments.js

--- src/CommentsManager/GetComments.js
+++ src/CommentsManager/GetComments.js
@@ -1,21 +1,16 @@
 import Comment from "../Entities/Comment";
 
-
 export default {
-
-    async loadComments(route, callback) {
-        let comments = [];
-        if(route!==null && route!==undefined && route!==""){
-           let commentsJson = await this.loadCommentsJson(route,callback);
-           console.log("Json comments file:" +JSON.stringify(commentsJson));
-           console.log(commentsJson.comments);
-           comments = this.parseJsonToEntity(route, commentsJson.comments);
-
-       }
-       return comments;
-
-
-    },
+  async loadComments(route, callback) {
+    let comments = [];
+    if (route !== null && route !== undefined && route !== "") {
+      let commentsJson = await this.loadCommentsJson(route, callback);
+      console.log("Json comments file:" + JSON.stringify(commentsJson));
+      console.log(commentsJson.comments);
+      comments = this.parseJsonToEntity(route, commentsJson.comments);
+    }
+    return comments;
+  },
 
   async loadCommentsJson(route, callback) {
     const auth = require("solid-auth-client");
@@ -24,36 +19,34 @@ export default {
 
     let session = await auth.currentSession();
 
-
-        if (!session || session.webId === undefined || session.webId === null) {
-            callback();
-        }
-
-        try {
-          let content = await fc.readFile(route);
-          let json = JSON.parse(content);
-          return json;
-        } catch (error1) {
-          console.log("Comment couldn't be parsed");
-        }
-
-    },
-
-    parseJsonToEntity(commentsUrl,comments){
-        let commentsArray = [];
-        //console.log(comments.length);
-        for (let i = 0; i< comments.length; i++) {
-
-    for (let i = 0; i < comments.length; i++) {
-      let newComment = new Comment(
-        commentsUrl,
-        comments[i].text,
-        comments[i].author,
-        comments[i].dateCreated
-      );
-      commentsArray.push(newComment);
+    if (!session || session.webId === undefined || session.webId === null) {
+      callback();
     }
 
-    return commentsArray;
-  }
-}
+    try {
+      let content = await fc.readFile(route);
+      let json = JSON.parse(content);
+      return json;
+    } catch (error1) {
+      console.log("Comment couldn't be parsed");
+    }
+  },
+
+  parseJsonToEntity(commentsUrl, comments) {
+    let commentsArray = [];
+    //console.log(comments.length);
+    for (let i = 0; i < comments.length; i++) {
+      for (let i = 0; i < comments.length; i++) {
+        let newComment = new Comment(
+          commentsUrl,
+          comments[i].text,
+          comments[i].author,
+          comments[i].dateCreated
+        );
+        commentsArray.push(newComment);
+      }
+
+      return commentsArray;
+    }
+  },
+};
