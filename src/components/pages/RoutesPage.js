84f87cd429f6e774ed27360369e3da10bafedbd7
fernandossmm <UO266754@uniovi.es>
2020-05-03 20:27:53 +0200

    Routes page now properly merged

--- src/components/pages/RoutesPage.js
+++ src/components/pages/RoutesPage.js
@@ -5,21 +5,25 @@ import BurgerMenu from "../generic_components/BurgerMenu";
 import SearchBar from "../generic_components/SearchBar";
 import CardLayout from "../generic_components/Card";
 import RouteDetails from "./RouteDetails";
-
+import { HashRouter as Router, Link } from "react-router-dom";
+import { Icon, Card, Image, Popup } from "semantic-ui-react";
 import * as cache from "caches/routeCache/RouteCache";
 
+import { sharedRoutesList } from "ShareManager/RetrieveRoute";
 
 class RoutesPage extends React.Component {
-
   constructor(props) {
     super(props);
-  this.state = {
-    loading: true,
-    routes: "",
-    search: "",
-    showDetails: false
-  };
+    this.state = {
+      loading: true,
+      routes: "",
+      sharedRoutes: "",
+      search: "",
+      showDetails: false,
+    };
   }
+
+  urls = [];
   updateSearch(event) {
     this.setState({ search: event.target.value.substr(0, 20) });
   }
@@ -39,27 +43,32 @@ class RoutesPage extends React.Component {
         });
       }
     });
+
     cache.default.setReload(false);
   }
 
-  viewDetails(route){
+  viewDetails(route) {
     cache.default.setSelectedDetails(route);
     this.setState({
-      showDetails: true
-    })
+      showDetails: true,
+    });
   }
 
-  getDetailsZone(){
-    return <RouteDetails showUpload = {() => {this.setState({
-      showDetails: !this.state.showDetails
-    })
-    }}/>
+  getDetailsZone() {
+    return (
+      <RouteDetails
+        showUpload={() => {
+          this.setState({
+            showDetails: !this.state.showDetails,
+          });
+        }}
+      />
+    );
   }
 
-    
   handleSession = () => {
     this.props.history.push("/login");
-  }
+  };
 
   viewLoaded = (routes, sharedRoutes) => {
     console.log(sharedRoutes)
@@ -69,75 +78,112 @@ class RoutesPage extends React.Component {
       );
     });
 
-  viewLoaded = routes => {
-      let filteredRoutes = routes.filter(ruta => {
-        return ruta.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
-      }
-    );
+    let filteredSharedRoutes = sharedRoutes.filter((ruta) => {
+      return (
+        ruta.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1
+      );
+    });
+
     return (
-
       <div className="bodyRoutes" id="outer-container">
-                  {this.state.showDetails ? this.getDetailsZone(): null}
+        {this.state.showDetails ? this.getDetailsZone() : null}
 
-      <main>
-          <BurgerMenu 
-          pageWrapId="page-wrap"
-          container="outer-container"
-          />
-        <div className="App routes" id="page-wrap">
-          <header className="bodyHeader"></header>
-          <section className="sectionRoutes">
-            
-            <SearchBar  value={this.state.search} 
-                        action={this.updateSearch.bind(this)}
-                        list="listRoute"
-            />
-            <ul className="listRoute">            
-              {filteredRoutes.map((item, index)=>{
-                return (
-                  <li id={"route"+index} key={index} className="liCard">
-                    <div className="routeListElementContainter">
-                      <CardLayout
-                        header={item.name}
-                        image="images/defaultRouteImage.svg"
-                        link = "/"
-                        className="linkRoute"
-
-                        description="Well, there should be a description..."
-                        action={e=>{cache.default.setSelected(routes[index])}}
-                        iconName="map"
-                        popupContent="Show route in map"
-                        
-                        detailsClassName="linkRoute"
-                        detailsLink="/routes"
-                        detailsAction={e=>{this.viewDetails(routes[index])}}
-                        detailsIconName="info"
-                        detailsPopupContent="Shows the messages and multimedia of the route"
-                      />
-                     
-                    </div>
-                  </li>
-                );
-              })} 
-            </ul>
-          </section>
-        </div>
-      </main>
-    </div>
+        <main>
+          <BurgerMenu pageWrapId="page-wrap" container="outer-container" />
+          <div className="App routes" id="page-wrap">
+            <header className="bodyHeader"></header>
+            <section className="sectionRoutes">
+              <SearchBar
+                value={this.state.search}
+                action={this.updateSearch.bind(this)}
+                list="listRoute"
+              />
+              <h1>My routes</h1>
+              <ul className="listRoute">
+                {filteredRoutes.map((item, index) => {
+                  return (
+                    <li id={"route" + index} key={index} className="liCard">
+                      <div className="routeListElementContainter">
+                        <CardLayout
+                          header={item.name}
+                          image="images/daddy.png"
+                          link="/"
+                          className="linkRoute"
+                          description={item.description}
+                          action={(e) => {
+                            cache.default.setSelected(routes[index]);
+                          }}
+                          iconName="map"
+                          popupContent="Show on map"
+                          detailsClassName="linkRoute"
+                          detailsLink="/routes"
+                          detailsAction={(e) => {
+                            this.viewDetails(routes[index]);
+                          }}
+                          detailsIconName="info"
+                          detailsPopupContent="Show messages and multimedia"
+                          shareIconName="share"
+                          shareAction={(e) => {
+                            cache.default.setSelectedToShare(item.url);
+                          }}
+                        />
+                      </div>
+                    </li>
+                  );
+                })}
+              </ul>
+              <h1>Shared routes</h1>
+              <ul className="listRoute">
+                {filteredSharedRoutes.map((item, index) => {
+                  return (
+                    <li id={"route" + index} key={index} className="liCard">
+                      <div className="routeListElementContainter">
+                        <CardLayout
+                          header={item.name}
+                          image="images/daddy.png"
+                          link="/"
+                          className="linkRoute"
+                          description={item.description}
+                          action={(e) => {
+                            cache.default.setSelected(sharedRoutes[index]);
+                          }}
+                          iconName="map"
+                          popupContent="Show on map"
+                          detailsClassName="linkRoute"
+                          detailsLink="/routes"
+                          detailsAction={(e) => {
+                            this.viewDetails(sharedRoutes[index]);
+                          }}
+                          detailsIconName="info"
+                          detailsPopupContent="Show messages and multimedia"
+                        />
+                      </div>
+                    </li>
+                  );
+                })}
+              </ul>
+            </section>
+          </div>
+        </main>
+      </div>
     );
-  }
+  };
   viewCharge = () => {
     return (
       <div className="bodyRoutes" id="outer-container">
         <CustomLoader />
       </div>
     );
-  }
+  };
   render() {
     const { loading } = this.state;
     return (
       <React.Fragment>
-        {loading ? <CustomLoader /> : this.viewLoaded(this.state.routes)}
+        {loading ? (
+          <CustomLoader />
+        ) : (
+          this.viewLoaded(this.state.routes, this.state.sharedRoutes)
+        )}
       </React.Fragment>
     );
   }
