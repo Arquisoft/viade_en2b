efeced9bdca1d294568165af70f278185edcf93f
Guillermo Fernández-Campoamor Fernández <uo264207@uniovi.es>
2020-05-07 00:27:27 +0200

    Final checks

--- src/components/generic_components/CustomLoader.js
+++ src/components/generic_components/CustomLoader.js
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactLoading from "react-loading";
-import 'assets/css/CustomLoader.css';
-export default class CustomLoader extends React.Component{
-    render(){
-        return (
-        <div className="containerLoader">
-            <ReactLoading 
-             type="bubbles" 
-             color="#fff" 
-             className="loader"
-             />
-        </div>
-        );
-    }
+import "assets/css/CustomLoader.css";
+export default class CustomLoader extends React.Component {
+  render() {
+    return (
+      <div className="containerLoader">
+        <ReactLoading type="spin" color="#509A50" className="loader" />
+      </div>
+    );
+  }
 }
