e11331d4701d882b1b23a7f897149302c52faf0c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-07 17:58:15 +0000

    Deploy Arquisoft/viade_en2b to github.com/Arquisoft/viade_en2b.git:gh-pages

---
+++ precache-manifest.fe12d3d8825e513239373979e0c689a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fea4f1e4c61b178f3e052802dc7a0456",
+    "url": "/viade_en2b/index.html"
+  },
+  {
+    "revision": "7d09da7dca44abc5128a",
+    "url": "/viade_en2b/static/css/2.7f55608a.chunk.css"
+  },
+  {
+    "revision": "92882fb5625ff93cd9ee",
+    "url": "/viade_en2b/static/css/main.768dbe4c.chunk.css"
+  },
+  {
+    "revision": "7d09da7dca44abc5128a",
+    "url": "/viade_en2b/static/js/2.73fde5df.chunk.js"
+  },
+  {
+    "revision": "a7d87bc4629b08efb260ccf6cdafd846",
+    "url": "/viade_en2b/static/js/2.73fde5df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92882fb5625ff93cd9ee",
+    "url": "/viade_en2b/static/js/main.f9eed44f.chunk.js"
+  },
+  {
+    "revision": "53665aa4ef9fd2d054b3",
+    "url": "/viade_en2b/static/js/runtime-main.d5e88302.js"
+  }
+]);
